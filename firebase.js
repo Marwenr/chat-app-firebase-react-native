1991ad2bb02db5e466a48649d1c84da960dce227
Marwenr <marwenrhim@gmail.com>
2023-09-18 13:14:22 +0200

    first commit

--- firebase.js
+++ firebase.js
@@ -7,14 +7,7 @@ import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyB0t6nPRooPdSinKFGvS51WtWSnME4Tqnc',
-  authDomain: 'chat-app-react-native-ff453.firebaseapp.com',
-  projectId: 'chat-app-react-native-ff453',
-  storageBucket: 'chat-app-react-native-ff453.appspot.com',
-  messagingSenderId: '94126856810',
-  appId: '1:94126856810:web:cde5ff90a3fc5dcff48813',
-};
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
